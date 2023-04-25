fde04ab613c91482e4f2b1ac1c53d21f711d4665
shokhabbos <urinovs999@gmail.com>
2023-04-25 16:54:28 +0500

    init

---
+++ src/components/Button/button.jsx
@@ -0,0 +1,9 @@
+export const Button = (a) => {
+    console.log(a)
+    return(
+        <>
+        {/* <button>Click me {name}</button> */}
+        
+        </>
+    )
+}
---
+++ src/components/card/card.jsx
@@ -0,0 +1,13 @@
+export const  Card = () => {
+  const title = "Picturesss "
+  return (
+    <div className="card"> 
+        <img src="https://picsum.photos/200/200" alt="" />
+        <div className="card-content">
+          <h2>{title}</h2>
+          <p>hellio</p>
+        </div>
+
+    </div>
+  )
+}
